5847a04d8deb43a45d657862fab7a968d9621dd1
Susheel Sahoo <susheel.sahoo@globallogic.com>
2024-05-25 10:10:31 +0530

    ss

--- app1/models/addition_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class AdditionRequest(BaseModel):
-    batchid: str
-    payload: List[List[int]]
-
-class AdditionResponse(BaseModel):
-    results: List[int]
--- app1/services/addition_service.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import Pool
-
-def add_pair(pair):
-    return sum(pair)
-
-def process_addition(numbers):
-    with Pool() as pool:
-        result = pool.map(add_pair, numbers)
-    return result
